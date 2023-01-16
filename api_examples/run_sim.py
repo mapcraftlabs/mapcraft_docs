b95f8b23f5695962c13c625f9280dc041afae954
fscottfoti <fscottfoti@gmail.com>
2023-01-16 08:04:18 -0800

    write response as geojson

--- api_examples/run_sim.py
+++ api_examples/run_sim.py
@@ -1,5 +1,7 @@
 import click
 from datetime import datetime
+import json
+import time
 import utils
 
 
@@ -14,15 +16,13 @@ def run_simulation(email, password):
 
     lab_id = "wsble2"
 
-    """
     response = utils.make_post_request(f"simulations/start/{lab_id}", token, dict(
         simulationName=f"API_SIM_{date_str}",
         simulationDescription="Test simulation using API",
         activeLayerScenarios={"Stations": "Baseline"},
     ))
-    """
 
-    simulation_id = "-NLt7OjIbwNczfKITJxA"  # response["simulationId"]
+    simulation_id = response["simulationId"]
     print(f"Simulation started with id: {simulation_id}")
 
     utils.poll_simulation_status_until_complete(lab_id, simulation_id, token)
@@ -31,7 +31,9 @@ def run_simulation(email, password):
     response = utils.make_get_request(
         f"combined_data/{lab_id}/Simulations/{simulation_id}", token
     )
-    print(response["data"])
+
+    with open(f"simulation_{simulation_id}_results.geojson", "w") as fp:
+        json.dump(response["data"], fp)
 
 
 if __name__ == "__main__":
