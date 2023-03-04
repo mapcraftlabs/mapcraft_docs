b2f4c7c376507a1bd041725e1d95da39eeb0fe7b
Martín Anzorena <martin.anzorena@gmail.com>
2023-03-04 00:29:34 -0300

    Add functions to utils.py

--- api_examples/utils.py
+++ api_examples/utils.py
@@ -1,6 +1,7 @@
-import requests
+import json
 import time
 
+import requests
 
 BASE_URL = "https://api.mapcraftlabs.com"
 # this API_KEY is not private - it's used to identify MapCraft accounts
@@ -54,3 +55,63 @@ def poll_simulation_status_until_complete(lab_id: str, simulation_id: str, token
             return
 
         time.sleep(5)
+
+
+def get_layer_data_url(lab_id: str, token: str) -> str:
+    """
+    Get URL where layer data file is stored.
+    """
+    response = requests.get(
+        f"{BASE_URL}/base_data/{lab_id}/Accessibility",
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 200
+    return json.loads(response.text)["url"]
+
+
+def download_layer_data_to_file(lab_id: str, file_name: str, token: str):
+    """
+    Download layer data to file.
+
+    file_name: full file path where will be downloaded.
+    """
+    url = get_layer_data_url(lab_id, token)
+    open("input.geojson", "wb").write(url)
+
+
+def upload_base_data_from_file(lab_id: str, layer_id: str, file_name: str, token: str):
+    """
+    Upload base data file to API.
+
+    file_name: full file path to be uploaded.
+    """
+    file = open(file_name, "rb")
+
+    response = requests.post(
+        f"{BASE_URL}/base_data/{lab_id}/{layer_id}",
+        headers={"Authorization": f"Bearer {token}"},
+        files={"file": file},
+    )
+    assert response.status_code == 200
+    print(response.text)
+    return
+
+
+def upload_join_csv_data_from_file(
+    lab_id: str, layer_id: str, scenario_id: str, file_name: str, token: str
+):
+    """
+    Upload join_csv data file to API.
+
+    file_name: full file path to be uploaded.
+    """
+    file = open(file_name, "rb")
+
+    response = requests.post(
+        f"{BASE_URL}/join_csv/{lab_id}/{layer_id}/{scenario_id}",
+        headers={"Authorization": f"Bearer {token}"},
+        files={"file": file},
+    )
+
+    assert response.status_code == 200
+    print(response.text)
